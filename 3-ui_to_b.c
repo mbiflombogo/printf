0ec77c7d74cd20071783318c8bed8c9c463b65f2
Owinnie <tillawinnie@gmail.com>
2022-04-17 04:32:59 -0700

    To Binary

--- 3-ui_to_b.c
+++ 3-ui_to_b.c
@@ -1,21 +1,6 @@
 #include "main.h"
 #include <stdio.h>
 /**
-  * to_base_len - len of dec nums converted to base
-  * @n: num of which we want len
-  * @base: convert to base from DEC
-  *
-  * Return: len
-  */
-unsigned int to_base_len(unsigned int n, int base)
-{
-	unsigned int i;
-
-	for (i = 0; n > 0; i++)
-		n = n / base;
-	return (i);
-}
-/**
   * _cpy - copy n bytes from src to dest
   * @dest: destination
   * @src: source
---
+++ to_base_len.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+  * to_base_len - len of dec nums converted to base
+  * @n: num of which we want len
+  * @base: convert to base from DEC
+  *
+  * Return: len
+  */
+unsigned int to_base_len(unsigned int n, int base)
+{
+	unsigned int i;
+
+	for (i = 0; n > 0; i++)
+		n = n / base;
+	return (i);
+}
